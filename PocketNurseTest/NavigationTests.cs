5a1f803cfe867e92d110aec9e59ec88db9c94ef2
Kevin Burton <rkevinburton@charter.net>
2018-06-04 15:59:42 -0500

    Established a basic smoke test to navigate to the Home page

---
+++ PocketNurseTest/NavigationTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketNurseAutomation.Pages;
+
+namespace PocketNurseTest
+{
+  [TestClass]
+  public class NavigationTests
+  {
+    [TestMethod]
+    public void TestHome()
+    {
+      HomePage.GoTo();
+      Assert.IsTrue(HomePage.IsAt);
+    }
+  }
+}
